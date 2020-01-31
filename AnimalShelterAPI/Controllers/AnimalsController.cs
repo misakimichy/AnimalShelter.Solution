03cb73bb670edb3d62f89633973c728df3b16bf3
Misaki Koonce <misaki.koonce@gmail.com>
2020-01-31 12:42:36 -0800

    Add GET, POST, PUT, DELETE functions.

--- AnimalShelterAPI/Controllers/AnimalsController.cs
+++ AnimalShelterAPI/Controllers/AnimalsController.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelterAPI.Models;
 
 namespace AnimalShelterAPI.Controllers
 {
@@ -10,36 +11,68 @@ namespace AnimalShelterAPI.Controllers
     [ApiController]
     public class AnimalsController : ControllerBase
     {
-        // // GET api/Animals
-        // [HttpGet]
-        // public ActionResult<IEnumerable<string>> Get()
-        // {
-        //     return new string[] { "value1", "value2" };
-        // }
+        private AnimalShelterAPIContext _db;
+        public AnimalsController(AnimalShelterAPIContext db)
+        {
+            _db = db;
+        }
 
-        // // GET api/Animals/5
-        // [HttpGet("{id}")]
-        // public ActionResult<string> Get(int id)
-        // {
-        //     return "value";
-        // }
+        // GET api/Animals
+        [HttpGet]
+        public ActionResult<IEnumerable<Animal>> Get(string species, string breed, int age, string gender)
+        {
+            var query = _db.Animals.AsQueryable();
 
-        // // POST api/Animals
-        // [HttpPost]
-        // public void Post([FromBody] string value)
-        // {
-        // }
+            if (species != null)
+            {
+                query = query.Where(entry => entry.Species == species);
+            }
+            if (breed != null)
+            {
+                query = query.Where(entry => entry.Breed == breed);
+            }
+            if (age != 0)
+            {
+                query = query.Where(entry => entry.Age == age);
+            }
+            if (gender != null)
+            {
+                query = query.Where(entry => entry.Gender == gender);
+            }
+            return query.ToList();
+        }
 
-        // // PUT api/Animals/5
-        // [HttpPut("{id}")]
-        // public void Put(int id, [FromBody] string value)
-        // {
-        // }
+        // GET api/Animals/id
+        [HttpGet("{id}")]
+        public ActionResult<Animal> Get(int id)
+        {
+            return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+        }
 
-        // // DELETE api/Animals/5
-        // [HttpDelete("{id}")]
-        // public void Delete(int id)
-        // {
-        // }
+        // POST api/Animals
+        [HttpPost]
+        public void Post([FromBody] Animal animal)
+        {
+            _db.Animals.Add(animal);
+            _db.SaveChanges();
+        }
+
+        // PUT api/Animals/id
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Animal animal)
+        {
+            animal.AnimalId = id;
+            _db.Entry(animal).State = EntityState.Modified;
+            _db.SaveChanges();
+        }
+
+        // DELETE api/Animals/id
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+            _db.Animals.Remove(animalToDelete);
+            _db.SaveChanges();
+        }
     }
 }
